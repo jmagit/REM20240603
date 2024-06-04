a2bf974b3da9bee404c4fc2aaa0d0067a585e6de
Javier <35864946+jmagit@users.noreply.github.com>
2024-06-04 10:38:54 +0200

    crud

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,8 +1,12 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.domains.entities.Actor;
+import com.example.domains.repositories.ActorRepository;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -11,9 +15,22 @@ public class DemoApplication implements CommandLineRunner {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Autowired
+	ActorRepository dao;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		System.err.println("Aplicación arrancada!!!!.");
+		
+//		dao.save(new Actor(0, "Pepito", "Grillo"));
+//		var item = dao.findById(204);
+//		if(item.isPresent()) {
+//			var actor = item.get();
+//			actor.setFirstName(actor.getFirstName().toUpperCase());
+//			dao.save(actor);
+//		}
+		dao.deleteById(205);
+		dao.findAll().forEach(System.out::println);
 	}
 
 }
