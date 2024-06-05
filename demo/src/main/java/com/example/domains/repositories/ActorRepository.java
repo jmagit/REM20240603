e34fb2e594ebd93765022095dfabfa1550f614f0
Javier <35864946+jmagit@users.noreply.github.com>
2024-06-05 13:25:49 +0200

    proyeciones

--- demo/src/main/java/com/example/domains/repositories/ActorRepository.java
+++ demo/src/main/java/com/example/domains/repositories/ActorRepository.java
@@ -2,12 +2,16 @@ package com.example.domains.repositories;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import com.example.domains.entities.Actor;
+import com.example.domains.entities.models.ActorDTO;
+import com.example.domains.entities.models.ActorShort;
 
 public interface ActorRepository extends JpaRepository<Actor, Integer>, JpaSpecificationExecutor<Actor> {
 	List<Actor> findTop5ByFirstNameStartingWithOrderByLastNameDesc(String prefijo);
@@ -18,5 +22,13 @@ public interface ActorRepository extends JpaRepository<Actor, Integer>, JpaSpeci
 	List<Actor> findByJPA(int inicial);
 	@Query(value = "select * from actor where actor_id > :inicial", nativeQuery = true)
 	List<Actor> findBySQL(int inicial);
-	
+
+	List<ActorDTO> queryByActorIdGreaterThan(int inicial);
+	List<ActorShort> readByActorIdGreaterThan(int inicial);
+	<T> List<T> searchByActorIdGreaterThan(int inicial, Class<T> tipo);
+
+	<T> List<T> findAllBy(Class<T> tipo);
+	<T> List<T> findAllBy(Sort orden, Class<T> tipo);
+	<T> Page<T> findAllBy(Pageable page, Class<T> tipo);
+
 }
